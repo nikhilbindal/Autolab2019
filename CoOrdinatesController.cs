02e361091370883785677e4ffdb2bd583c6fa867
nikhilbindal <56270982+nikhilbindal@users.noreply.github.com>
2019-12-09 15:16:57 +0530

    Add files via upload

---
+++ CoOrdinatesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class CoOrdinatesController : Controller
+    {
+        // GET: CoOrdinates
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
